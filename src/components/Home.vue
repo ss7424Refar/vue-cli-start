25c1bf051ed52313306869f3ef66fb1fe86544fc
ss7424Refar <812647742@qq.om>
2019-05-27 10:36:17 +0800

    [Update] remove container_20190527

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="home" class="container">
+  <div id="home">
 
     <!--<div class="text-center my-3">-->
       <!--<b-button v-b-popover.hover="'I am popover content!'" title="Popover Title">Hover Me</b-button>-->
